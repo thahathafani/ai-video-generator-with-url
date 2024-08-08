9adb2e6d0dcff165c1191ed42b8e6fa92eef5c0a
thahathafani <thahafazzin@gmail.com>
2024-08-08 19:21:37 +0530

    url setup

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,24 @@
 const express = require("express");
+const uniqid = require("uniqid");
+const fs = require('fs');
 const app = express();
 
 app.get('/', (req, res) => {
     res.send('server ');
-})
+});
 
-app.listen(8000, () => console.log('server started...'))
+app.get('/story', (req, res) => {
+    const url = req.query.url;
+    const id = uniqid();
+    const dir = id;  // Define the directory name
+    const path = './stories/' + dir;
+    fs.mkdirSync(path, { recursive: true });
+
+    console.log({
+        url,
+    });
+
+    return res.json('ok');
+});
+
+app.listen(8000, () => console.log('server started...'));
